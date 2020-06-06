ecc9c55ec320f024bcb24a8953cb268c5903fd6f
unknown <kr.vivek.kr@gmail.com>
2020-06-06 10:10:33 +0530

    first commit

---
+++ schema.py
@@ -0,0 +1,27 @@
+import graphene
+import json
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, info):
+        return "world"
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+    {
+        hello
+    }
+    '''
+)
+
+print(result.data.items())
+print(result.data['hello'])
+
+dictResult = dict(result.data.items())
+print(json.dumps(dictResult))
+print(json.dumps(dictResult,indent=2))
+
+
